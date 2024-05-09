11661dd363a1ea7b8c9b527a6330c154b6609d79
Justin Reese <justaddcoffee@gmail.com>
2024-05-09 13:58:59 -0700

    Make results tsv

--- src/malco/post_process/post_process_results_format.py
+++ src/malco/post_process/post_process_results_format.py
@@ -1,13 +1,16 @@
 import json
+import os
 from pathlib import Path
 from typing import List
 
+import pandas as pd
+import yaml
 from pheval.post_processing.post_processing import PhEvalGeneResult, generate_pheval_result
 from pheval.utils.file_utils import all_files
 from pheval.utils.phenopacket_utils import GeneIdentifierUpdater, create_hgnc_dict
 
 
-def read_raw_result(raw_result_path: Path) -> List[dict]:
+def read_raw_result_yaml(raw_result_path: Path) -> List[dict]:
     """
     Read the raw result file.
 
@@ -15,105 +18,47 @@ def read_raw_result(raw_result_path: Path) -> List[dict]:
         raw_result_path(Path): Path to the raw result file.
 
     Returns:
-        List[dict]: Contents of the raw result file.
+        dict: Contents of the raw result file.
     """
-    with open(raw_result_path) as raw_result:
-        raw_result_data = json.load(raw_result)
-    raw_result.close()
+    with open(raw_result_path, 'r') as raw_result:
+        raw_result_data = yaml.safe_load(raw_result)
     return raw_result_data
 
 
-class ConvertToPhEvalResult:
-    """Class to convert the raw result file to PhEvalGeneResult."""
-
-    def __init__(self, raw_result: List[dict], gene_identifier_updator: GeneIdentifierUpdater):
-        """
-        Initialise the ConvertToPhEvalResult class.
-
-        Args:
-            raw_result (List[dict]): Contents of the raw result file.
-            gene_identifier_updator (GeneIdentifierUpdater): GeneIdentifierUpdater object.
-
-        """
-        self.raw_result = raw_result
-        self.gene_identifier_updator = gene_identifier_updator
-
-    @staticmethod
-    def _obtain_score(result_entry: dict) -> float:
-        """
-        Obtain the score from the result entry.
-
-        Args:
-            result_entry (dict): Contents of the result entry.
-
-        Returns:
-            float: The score.
-        """
-        return result_entry["score"]
-
-    @staticmethod
-    def _obtain_gene_symbol(result_entry: dict) -> str:
-        """
-        Obtain the gene symbol from the result entry.
-
-        Args:
-            result_entry (dict): Contents of the result entry.
-
-        Returns:
-            str: The gene symbol.
-        """
-        return result_entry["gene_symbol"]
-
-    def obtain_gene_identifier(self, result_entry: dict) -> str:
-        """
-        Obtain the gene identifier from the result entry.
-
-        Args:
-            result_entry (dict): Contents of the result entry.
-
-        Returns:
-            str: The gene identifier.
-        """
-        return self.gene_identifier_updator.find_identifier(self._obtain_gene_symbol(result_entry))
-
-    def extract_pheval_gene_requirements(self) -> List[PhEvalGeneResult]:
-        """
-        Extract the data required to produce PhEval gene output.
-
-        Returns:
-            List[PhEvalGeneResult]: List of PhEvalGeneResult objects.
-        """
-        pheval_result = []
-        for result_entry in self.raw_result:
-            pheval_result.append(
-                PhEvalGeneResult(
-                    gene_symbol=self._obtain_gene_symbol(result_entry),
-                    gene_identifier=self.obtain_gene_identifier(result_entry),
-                    score=self._obtain_score(result_entry),
-                )
-            )
-        return pheval_result
-
-
-def create_standardised_results(raw_results_dir: Path, output_dir: Path) -> None:
+def create_standardised_results(raw_results_dir: Path, output_dir: Path,
+                                output_file_name: str = "results.tsv") -> None:
     """
-    Create PhEval gene tsv output from raw results.
+    (via enock.Niyonkuru@jax.org)
+    Load a YAML file, create a DataFrame with columns 'label', 'term', and 'score', and
+    save it to a TSV file.
 
-    Args:
-        raw_results_dir (Path): Path to the raw results directory.
-        output_dir (Path): Path to the output directory.
+    Parameters:
+    yaml_file_path (str): The path to the YAML file.
+    output_file_path (str): The path where the TSV file will be saved.
+
+    Returns:
+    pd.DataFrame: The DataFrame created from the YAML data.
     """
-    gene_identifier_updator = GeneIdentifierUpdater(
-        gene_identifier="ensembl_id", hgnc_data=create_hgnc_dict()
-    )
     for raw_result_path in all_files(raw_results_dir):
-        raw_result = read_raw_result(raw_result_path)
-        pheval_result = ConvertToPhEvalResult(
-            raw_result, gene_identifier_updator
-        ).extract_pheval_gene_requirements()
-        generate_pheval_result(
-            pheval_result=pheval_result,
-            sort_order_str="DESCENDING",
-            output_dir=output_dir,
-            tool_result_path=raw_result_path,
-        )
+
+        raw_result = read_raw_result_yaml(raw_result_path)
+
+        extracted_object = raw_result.get("extracted_object")
+        data = []
+        if extracted_object:
+            label = extracted_object.get('label')
+            terms = extracted_object.get('terms')
+            if terms:
+                num_terms = len(terms)
+                score = [1 / (i + 1) for i in range(num_terms)]
+                for term, scr in zip(terms, score):
+                    data.append({'label': label, 'term': term, 'score': scr})
+
+        # Create DataFrame
+        df = pd.DataFrame(data)
+
+        # Save DataFrame to TSV
+        df.to_csv(os.path.join(output_dir, output_file_name), sep='\t', index=False)
+
+        return df
+
