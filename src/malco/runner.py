13c2f2d05b79932776f3130914dd7deccb49d45c
Leonardo local Kubuntu 22.04 <leonardochimirri94@gmail.com>
2024-05-10 20:58:59 +0200

    fixed labeling for spanish, should make it take any language with careful wildcarding

--- src/malco/runner.py
+++ src/malco/runner.py
@@ -27,9 +27,11 @@ class MalcoRunner(PhEvalRunner):
     import zipfile
     import os
 
-    def prepare(self,
-                phenopacket_zip_url="https://github.com/monarch-initiative/phenopacket-store/releases/download/0.1.8/all_phenopackets.zip",
-                phenopacket_dir="phenopacket-store"):
+    def prepare(
+        self,
+        phenopacket_zip_url="https://github.com/monarch-initiative/phenopacket-store/releases/download/0.1.8/all_phenopackets.zip",
+        phenopacket_dir="phenopacket-store",
+    ):
         """
         Pre-process any data and inputs necessary to run the tool.
         """
@@ -58,7 +60,7 @@ class MalcoRunner(PhEvalRunner):
         Post-process the raw output into PhEval standardised TSV output.
         """
         print("post processing results to PhEval standardised TSV output.")
-        post_process(raw_results_dir=self.raw_results_dir, output_dir=self.output_dir)
+        # post_process(raw_results_dir=self.raw_results_dir, output_dir=self.output_dir)
 
         if make_plot:
             self._make_plot()
@@ -70,62 +72,63 @@ class MalcoRunner(PhEvalRunner):
 
         # Download the phenopacket release zip file
         response = requests.get(phenopacket_zip_url)
-        zip_path = os.path.join(self.input_dir, 'all_phenopackets.zip')
-        with open(zip_path, 'wb') as f:
+        zip_path = os.path.join(self.input_dir, "all_phenopackets.zip")
+        with open(zip_path, "wb") as f:
             f.write(response.content)
         print("Download completed.")
 
         # Unzip the phenopacket release zip file
-        with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+        with zipfile.ZipFile(zip_path, "r") as zip_ref:
             zip_ref.extractall(phenopacket_store_path)
         print("Unzip completed.")
 
-    def _make_plot(self, prompt_dir="prompts",
-                   correct_answer_file="correct_results.tsv"):
-        # Read in results TSVs from self.output_dir that match glob results*tsv
+    def _make_plot(self, prompt_dir="prompts", correct_answer_file="correct_results.tsv"):
+        # Read in results TSVs from self.output_dir that match glob results*tsv --> TODO Leo: make more robust, had other results*tsv files from previous testing
         results_data = []
         results_files = []
         for subdir, dirs, files in os.walk(self.output_dir):
             for filename in files:
                 if filename.startswith("result") and filename.endswith(".tsv"):
                     file_path = os.path.join(subdir, filename)
-                    df = pd.read_csv(file_path, sep='\t')
+                    df = pd.read_csv(file_path, sep="\t")
                     results_data.append(df)
                     # Append both the subdirectory relative to output_dir and the filename
                     results_files.append(os.path.relpath(file_path, self.output_dir))
         # Read in correct answers from prompt_dir
         answers_path = os.path.join(os.getcwd(), prompt_dir, correct_answer_file)
-        answers = pd.read_csv(answers_path, sep='\t', header=None,
-                              names=['description', 'term', 'label'])
+        answers = pd.read_csv(
+            answers_path, sep="\t", header=None, names=["description", "term", "label"]
+        )
 
         # Mapping each label to its correct term
-        label_to_correct_term = answers.set_index('label')['term'].to_dict()
+        label_to_correct_term = answers.set_index("label")["term"].to_dict()
 
         # Calculate the Mean Reciprocal Rank (MRR) for each file
         mrr_scores = []
         for df in results_data:
             # For each label in the results file, find if the correct term is ranked
-            df['rank'] = df.groupby('label')['score'].rank(ascending=False,
-                                                           method='first')
-            df['correct_term'] = df['label'].map(label_to_correct_term)
+            print(df)
+            df["rank"] = df.groupby("label")["score"].rank(ascending=False, method="first")
+            label_4_non_eng = df["label"].str.replace("json_es-prompt", "json_en-prompt")
+            df["correct_term"] = label_4_non_eng.map(label_to_correct_term)
             # df['term'] is sometimes a Mondo ID
             # df['correct_term'] is always an OMIM
             # TODO: call OAK and get OMIM IDs for df['term'] and see if df['correct_term'] is one of them
             # if so, df["is_correct"] = True
-            df['is_correct'] = (df['term'] == df['correct_term'])
+            df["is_correct"] = df["term"] == df["correct_term"]
 
             # Calculate reciprocal rank
-            df['reciprocal_rank'] = df.apply(
-                lambda row: 1 / row['rank'] if row['is_correct'] else 0, axis=1)
-
+            df["reciprocal_rank"] = df.apply(
+                lambda row: 1 / row["rank"] if row["is_correct"] else 0, axis=1
+            )
             # Calculate MRR for this file
-            mrr = df.groupby('label')['reciprocal_rank'].max().mean()
+            mrr = df.groupby("label")["reciprocal_rank"].max().mean()
             mrr_scores.append(mrr)
 
         # Plotting the results
         sns.barplot(x=results_files, y=mrr_scores)
         plt.xticks(rotation=90)  # Rotate labels for better readability
-        plt.xlabel('Results File')
-        plt.ylabel('Mean Reciprocal Rank (MRR)')
-        plt.title('MRR of Correct Answers Across Different Results Files')
+        plt.xlabel("Results File")
+        plt.ylabel("Mean Reciprocal Rank (MRR)")
+        plt.title("MRR of Correct Answers Across Different Results Files")
         plt.show()
