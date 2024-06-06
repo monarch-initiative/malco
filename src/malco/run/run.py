47019870c7e5eb2fcc3cebf01931e4c4333c3c33
Justin Reese <justaddcoffee@gmail.com>
2024-06-06 23:28:41 +0200

    Tidy up, rename method

--- src/malco/run/run.py
+++ src/malco/run/run.py
@@ -1,9 +1,9 @@
-import os
 from pathlib import Path
 import multiprocessing
 import subprocess
 
-def run_analysis(lang, raw_results_dir, input_dir):
+
+def call_ontogpt(lang, raw_results_dir, input_dir):
     command = (
         f"ontogpt -v run-multilingual-analysis "
         f"--output={raw_results_dir}/{lang}/results.yaml "  # save raw OntoGPT output
@@ -14,6 +14,7 @@ def run_analysis(lang, raw_results_dir, input_dir):
     process = subprocess.Popen(command, shell=True)
     process.communicate()
     print(f"Finished command for {lang}")
+
 
 def run(testdata_dir: Path,
         raw_results_dir: Path,
@@ -30,10 +31,9 @@ def run(testdata_dir: Path,
         langs: Tuple of languages.
         max_workers: Maximum number of worker processes to use.
     """
-    mydir = os.getcwd()
 
     if max_workers is None:
         max_workers = multiprocessing.cpu_count()
 
     with multiprocessing.Pool(processes=max_workers) as pool:
-        pool.starmap(run_analysis, [(lang, raw_results_dir, input_dir) for lang in langs])
+        pool.starmap(call_ontogpt, [(lang, raw_results_dir, input_dir) for lang in langs])
