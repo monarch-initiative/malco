18ba888d594deb2e4ef85b21ff37b82b17f4795f
Leonardo macOS <leonardochimirri94@gmail.com>
2024-06-25 15:26:37 +0200

    added saving of dataframe of aggregated top n results, used for the bar plot

--- src/malco/post_process/generate_plots.py
+++ src/malco/post_process/generate_plots.py
@@ -23,7 +23,6 @@ def make_plots(plot_data_file, plot_dir, languages, num_ppkt, topn_file):
     plt.title("MRR of Correct Answers Across Different Results Files")
     plot_path = plot_dir /  (str(len(languages)) + "_langs_" + str(num_ppkt) + "ppkt.png")
     plt.savefig(plot_path)
-    plt.show()
 
     # Plotting bar-plots with top<n> ranks
     df = pd.read_csv(topn_file, delimiter='\t')
@@ -35,6 +34,9 @@ def make_plots(plot_data_file, plot_dir, languages, num_ppkt, topn_file):
     
     df_aggr = pd.DataFrame()
     df_aggr = pd.melt(df, id_vars="lang", value_vars=["top1", "top3", "top5", "top10", "not_found"], var_name="Rank_in", value_name="counts")
+    bar_data_file = plot_dir / "topn_aggr.tsv"
+    df_aggr.to_csv(bar_data_file, sep='\t', index=False)
+
     sns.barplot(x="Rank_in", y="counts", data = df_aggr, hue = "lang")
 
     plt.xlabel("Number of Ranks")
