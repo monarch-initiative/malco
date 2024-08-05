739f346901ea57695881d57833f74820f2dca8b6
Leonardo macOS <leonardochimirri94@gmail.com>
2024-08-05 13:39:52 +0200

    mini script looking at how many times in published output the grounding to mondo succeeded

---
+++ src/malco/count_grounding_failures.py
@@ -0,0 +1,19 @@
+# Quick check how often the grounding failed
+import pandas as pd
+mfile = "outputdir_all_2024_07_04/en/results.tsv"
+
+df = pd.read_csv(
+        mfile, sep="\t" #, header=None, names=["description", "term", "label"]
+    )
+
+terms = df["term"]
+counter = 0
+grounded = 0
+for term in terms:
+    if term.startswith("MONDO"):
+        grounded += 1
+    else:
+        counter += 1
+
+print(counter)
+print(grounded)
